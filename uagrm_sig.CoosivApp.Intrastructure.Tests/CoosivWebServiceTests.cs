5cb5c89db8335006f88bace3014af585a57beb84
Róger Roca Arano <git@rogerroca.me>
2024-12-06 10:31:53 -0400

    test: write unit test for GetRoutes in CoosivWebService

---
+++ uagrm_sig.CoosivApp.Intrastructure.Tests/CoosivWebServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using uagrm_sig.CoosivApp.Infrastructure.CoosivClient;
+using Xunit.Abstractions;
+
+namespace uagrm_sig.CoosivApp.Intrastructure.Tests;
+
+public class CoosivWebServiceTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly Mock<IHttpClientFactory> _httpClientFactoryMock;
+    private readonly CoosivWebService _coosivWebService;
+
+    public CoosivWebServiceTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+        _httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        var httpClient = new HttpClient();
+        _httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+        var baseUrl = "http://190.171.244.211:8080/wsVarios/wsBs.asmx";
+        var ns = "http://tempuri.org/";
+        _coosivWebService = new CoosivWebService(_httpClientFactoryMock.Object, baseUrl, ns);
+    }
+
+    [Fact]
+    public async Task GetRoutes_ShouldReturnNotNull()
+    {
+        // Arrange
+        // Configura el mock y los datos esperados aquí
+
+        // Act
+        var result = await _coosivWebService.GetRoutes();
+
+        // Assert
+        // Verifica que el resultado sea el esperado
+        _testOutputHelper.WriteLine(result.Serialize());
+        Assert.NotNull(result);
+    }
+}
