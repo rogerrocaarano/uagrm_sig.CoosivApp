ff60ca054825cf47faec0de49f85a4eed355d565
Róger Roca Arano <git@rogerroca.me>
2024-12-09 00:02:04 -0400

    feat: Define domain for service cancellation operation

---
+++ uagrm_sig.CoosivApp.Domain/Entities/ServiceCancellation.cs
@@ -0,0 +1,9 @@
+﻿namespace uagrm_sig.CoosivApp.Domain.Entities;
+
+public class ServiceCancellation
+{
+    public ServiceAccount ServiceAccount { get; set; }
+    public bool CancellationCompleted { get; set; }
+    public DateTime TimeOfCancellation { get; set; }
+    public string? Observation { get; set; }
+}
