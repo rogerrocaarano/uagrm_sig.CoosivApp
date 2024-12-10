7f620b70987faf23e1fc5d691ae2c8d3cd0f3690
Róger Roca Arano <git@rogerroca.me>
2024-12-09 20:59:42 -0400

    doc: W3Corte_UpdateCorteRequest.cs reference

--- uagrm_sig.CoosivApp.Infrastructure/DTOs/CoosivWebService/W3Corte_UpdateCorteRequest.cs
+++ uagrm_sig.CoosivApp.Infrastructure/DTOs/CoosivWebService/W3Corte_UpdateCorteRequest.cs
@@ -4,14 +4,14 @@ namespace uagrm_sig.CoosivApp.Infrastructure.DTOs.CoosivWebService;
 
 public class W3Corte_UpdateCorteRequest : ISoapRequest
 {
-    public int LiNcoc { get; set; }
-    public int LiCemc { get; set; }
-    public DateTime LdFcor { get; set; }
-    public int LiPres { get; set; }
-    public int LiCobc { get; set; }
-    public int LiLcor { get; set; }
-    public int LiNofn { get; set; }
-    public string LsAppName { get; set; }
+    public int LiNcoc { get; set; } // Número de corte
+    public int LiCemc { get; set; } // Código empleado que realiza el corte
+    public DateTime LdFcor { get; set; } // Fecha del corte
+    public int LiPres { get; set; } // Código grupo observación
+    public int LiCobc { get; set; } // Código de observación
+    public int LiLcor { get; set; } // ???
+    public int LiNofn { get; set; } // Código de oficina
+    public string LsAppName { get; set; } // Nombre de la aplicación
     
     public string ToSoapBody()
     {
